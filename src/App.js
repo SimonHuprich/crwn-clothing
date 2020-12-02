e8920f9a2c0229700000e7f17033ee8cccf171cd
Richard Goedel <richard.goedel.contractor@rle.de>
2020-12-02 11:50:24 +0100

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Homepage from "./homepage.component";
+import "./homepage.style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
