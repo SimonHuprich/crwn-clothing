0bf8bf7de6e83a719fe5ab1c04c89133f6446997
Richard Goedel <richard.goedel.contractor@rle.de>
2020-12-03 13:17:03 +0100

    create React Router

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./homepage.style.scss";
 import Directory from "../../components/directory/directory.component";
 
-const Homepage = () => (
+const Homepage = ({ history }) => (
   <div className="homepage">
     <Directory />
   </div>
